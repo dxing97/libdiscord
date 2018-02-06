9c7da2baae9d6a1bae0ea4efbc6421a0023f5909
dxing97 <danielxing97@gmail.com>
2018-02-06 12:31:12 -0500

    compartmentalized logging a little bit

--- lib/log.c
+++ lib/log.c
@@ -72,6 +72,50 @@ const char *ld_log_level_string(unsigned long ll) {
 void _ld_log(unsigned long ll, unsigned long enabled_levels, const char *log_message, va_list arg){
     if((ll & enabled_levels) == 0)
         return;
+//    time_t raw_time;
+//    struct tm *current_time;
+    char *message;
+//    char *time_string;
+//    //[2017/08/18 19:19:18]
+//
+//    //time part
+//    time_string = malloc(30 * sizeof(char));
+//    time(&raw_time);
+//    current_time = localtime(&raw_time);
+//    asctime_r(current_time, time_string);
+//    time_string[24] = '\0';
+//
+//    //message part
+//    char msg[16000]; //watch for buffer overflows here
+//    int ret;
+//
+//    ret = vsprintf(msg, log_message, arg);
+//
+//    if(ret < 0){
+//        perror("libdiscord: couldn't create log message");
+//        return;
+//    }
+//
+//    message = malloc(strlen(time_string) + strlen(ld_log_level_string(ll)) + strlen(msg) + 15);
+//    sprintf(message, "[%s] LD_%s: %s", time_string, ld_log_level_string(ll), msg);
+//    fprintf(stderr, "%s\n", message);
+//
+//    free(time_string);
+    message = _ld_log_generate_string(ll, log_message, arg);
+    if(message == NULL) {
+        perror("libdiscord counldn't print an error message");
+        return;
+    }
+    fprintf(stderr, "%s\n", message);
+    free(message);
+}
+
+/*
+ * generates a log string
+ * string memory is allocated by this function, it must be freed afterward
+ * todo: make it read-only (and avoid the free)
+ */
+char *_ld_log_generate_string(unsigned long ll, const char *log_message, va_list arg) {
     time_t raw_time;
     struct tm *current_time;
     char *message, *time_string;
@@ -92,14 +136,10 @@ void _ld_log(unsigned long ll, unsigned long enabled_levels, const char *log_mes
 
     if(ret < 0){
         perror("libdiscord: couldn't create log message");
-        return;
+        return NULL;
     }
 
     message = malloc(strlen(time_string) + strlen(ld_log_level_string(ll)) + strlen(msg) + 15);
     sprintf(message, "[%s] LD_%s: %s", time_string, ld_log_level_string(ll), msg);
-    fprintf(stderr, "%s\n", message);
-
-    free(time_string);
-    free(message);
+    return message;
 }
-
