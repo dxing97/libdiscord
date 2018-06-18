e47fb5debf1834c3fc3671aa1ddaeac537f2967b
dxing97 <danielxing97@gmail.com>
2018-06-17 21:13:16 -0400

    fixed script versioning problem

--- scripts/compile-install-libwebsockets.sh
+++ scripts/compile-install-libwebsockets.sh
@@ -2,16 +2,17 @@
 
 #libwebsockets v3.0.0
 
-LWS_VERSION=3.0.0
+LWS_VERSION="3.0.0"
 
 echo "Installing libwebsockets"
 git clone https://github.com/warmcat/libwebsockets
 cd libwebsockets
-    git checkout v$(LWS_VERSION)
-    mkdir build && cd build
-        cmake .. -DCMAKE_BUILD_TYPE=Debug -DLWS_WITH_LIBUV=ON -DLWS_WITH_LATENCY=ON -DLWS_WITHOUT_EXTENSIONS=OFF -DLWS_WITH_PLUGINS=ON -DLWS_WITH_ZLIB=ON -DLWS_WITHOUT_DAEMONIZE=ON
+    git checkout v"$LWS_VERSION"
+    mkdir build
+    cd build
+        cmake .. -DCMAKE_BUILD_TYPE=DEBUG -DLWS_WITH_LIBUV=ON -DLWS_WITH_LATENCY=ON -DLWS_WITHOUT_EXTENSIONS=OFF -DLWS_WITH_PLUGINS=ON -DLWS_WITH_ZLIB=ON -DLWS_WITHOUT_DAEMONIZE=ON
         make
-        sudo checkinstall --pkgname libwebsockets-dev --pkgversion="$(LWS_VERSION)"
+        sudo checkinstall --pkgname libwebsockets-dev --pkgversion="$LWS_VERSION"
         sudo ldconfig
     cd ..
 cd ..
