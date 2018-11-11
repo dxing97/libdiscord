931ab5ff0a3c35f6604e672247f284c6f5a6afa3
dxing97 <danielxing97@gmail.com>
2018-11-10 17:38:07 -0800

    update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-LWS_VERSION=2.4.2
-ULFIUS_VERSION=2.3.6
+LWS_VERSION=3.0.1
+#ULFIUS_VERSION=2.3.6
 ## Uber basic installation script
 # WIP, always examine the source before arbitrarily running someone else's scripts
 
@@ -26,14 +26,14 @@ if [ $(id -u) = 0 ]; then
 fi
 
 sudo apt update && sudo apt upgrade
-sudo apt install checkinstall libmicrohttpd-dev libjansson-dev libcurl4-gnutls-dev\
-                 libgnutls28-dev libgcrypt20-dev git \
+sudo apt install checkinstall libmicrohttpd-dev libjansson-dev libcurl4-openssl1.0-dev\
+                 git \
                  make cmake gcc libssl-dev libconfig-dev\
-                 zlib1g-dev libssl-dev libcurl4-gnutls-dev libuv1-dev libsystemd-dev
+                 zlib1g-dev libssl-dev libcurl4-gnutls-dev libuv1-dev
 #sudo apt install libulfius-dev
 #sudo apt install libwebsockets-dev
 #
-echo "Installing libulfius"
+#echo "Installing libulfius"
 ## Old makefile based compilation
 #git clone https://github.com/babelouest/ulfius.git
 #cd ulfius
@@ -48,25 +48,26 @@ echo "Installing libulfius"
 #        cd ../..
 #    make && sudo checkinstall --pkgname libulfius-sdev --install=yes
 #cd ..
-git clone https://github.com/babelouest/ulfius.git
-cd ulfius
-    git checkout $(ULFIUS_VERSION)
-    git submodule update --init
-    mkdir build && cd build
-        cmake ..
-        make
-        sudo checkinstall --pkgname libulfius-dev --pkgversion="$(ULFIUS_VERSION)"
-        sudo ldconfig
-    cd ..
-cd ..
+#git clone https://github.com/babelouest/ulfius.git
+#cd ulfius
+#    git checkout $(ULFIUS_VERSION)
+#    git submodule update --init
+#    mkdir build && cd build
+#        cmake ..
+#        make
+#        sudo checkinstall --pkgname libulfius-dev --pkgversion="$(ULFIUS_VERSION)"
+#        sudo ldconfig
+#    cd ..
+#cd ..
 
 # need to compile for v2.4.2 (ringbuffer APIs)
 echo "Installing libwebsockets"
 git clone https://github.com/warmcat/libwebsockets
 cd libwebsockets
     git checkout v$(LWS_VERSION)
-    mkdir build && cd build
-        cmake .. -DCMAKE_BUILD_TYPE=Debug -DLWS_WITH_LIBUV=ON -DLWS_WITH_LATENCY=ON
+    mkdir build
+    cd build
+        cmake .. -DCMAKE_BUILD_TYPE=Debug -DLWS_WITH_LIBUV=ON -DLWS_WITH_LATENCY=ON -DLWS_WITH_IPV6=ON -DLWS_WITH_PLUGINS=ON
         make
         sudo checkinstall --pkgname libwebsockets-dev --pkgversion="$(LWS_VERSION)"
         sudo ldconfig
